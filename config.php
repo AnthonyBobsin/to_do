be0450fc2d4ce84e10fba7974fb38b9a8a3c57e9
AnthonyBobsin <bobsinj@gmail.com>
2014-12-26 12:40:59 +0800

    Switched to heroku config

--- config.php
+++ config.php
@@ -7,9 +7,9 @@
 	$db_name = "heroku_1ef8938ff6dee0a";
 
 	//Localhost
-	$db_host = $_SERVER['HTTP_HOST'];
+	/*$db_host = $_SERVER['HTTP_HOST'];
 	$db_user = 'anthony';
 	$db_pass = 'submarine';
-	$db_name = 'db';
+	$db_name = 'db';*/
 
 ?>
