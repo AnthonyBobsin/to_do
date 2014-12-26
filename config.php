29e15c42296aa741b0a9c4139472cf4fd3b51c5d
AnthonyBobsin <bobsinj@gmail.com>
2014-12-26 12:04:15 +0800

    Experimenting with ClearDB

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 	//Heroku
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$db_host = url["$_SERVER['HTTP_HOST']"];
-	$db_user = url["anthony"];
-	$db_pass = url["submarine"];
-	$db_name = substr($url["db"], 1);;
+	$db_host = url["us-cdbr-iron-east-01.cleardb.net"];
+	$db_user = url["b347ec276fb80c"];
+	$db_pass = url["ac01ccff"];
+	$db_name = substr($url["heroku_1ef8938ff6dee0a"], 1);;
 
 	//Localhost
 	/*$db_host = $_SERVER['HTTP_HOST'];
