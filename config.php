d5d7212da958f21b2889449a0c5db51f1d5cbffd
AnthonyBobsin <bobsinj@gmail.com>
2014-12-26 11:56:07 +0800

    Experimenting with cleardb

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php
 
-	$db_host = $_SERVER['HTTP_HOST'];
+	//Heroku
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$db_host = url["$_SERVER['HTTP_HOST']"];
+	$db_user = url["anthony"];
+	$db_pass = url["submarine"];
+	$db_name = substr($url["db"], 1);;
+
+	//Localhost
+	/*$db_host = $_SERVER['HTTP_HOST'];
 	$db_user = 'anthony';
 	$db_pass = 'submarine';
-	$db_name = 'db';
+	$db_name = 'db';*/
 
 ?>
