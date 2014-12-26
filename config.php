34a8a2f1419d8f43ce960d7262739b613e6b3223
AnthonyBobsin <bobsinj@gmail.com>
2014-12-26 12:39:58 +0800

    Switched to heroku config

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 	//Heroku
-	/*$db_host = "us-cdbr-iron-east-01.cleardb.net";
+	$db_host = "us-cdbr-iron-east-01.cleardb.net";
 	$db_user = "b347ec276fb80c";
 	$db_pass = "ac01ccff";
-	$db_name = "heroku_1ef8938ff6dee0a";*/
+	$db_name = "heroku_1ef8938ff6dee0a";
 
 	//Localhost
 	$db_host = $_SERVER['HTTP_HOST'];
